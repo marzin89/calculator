2e079df91e5914fa8a31054c2fb5aac8c9b212bf
Marco <dead_legend@hotmail.com>
2020-07-28 23:56:17 +0200

    Define and declare variables for buttons and inputs. Change some id names in the html.

---
+++ calculator.js
@@ -0,0 +1,85 @@
+let expression = document.getElementById('expression');
+
+let result = document.getElementById('result');
+
+const clearEntry = document.getElementById('clear-entry');
+
+clearEntry.addEventListener('click', );
+
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', );
+
+const remainder = document.getElementById('remainder');
+
+remainder.addEventListener('click', );
+
+const division = document.getElementById('division');
+
+division.addEventListener('click', );
+
+const multiplication = document.getElementById('multiplication');
+
+multiplication.addEventListener('click', );
+
+const subtraction = document.getElementById('subtraction');
+
+subtraction.addEventListener('click', );
+
+const addition = document.getElementById('addition');
+
+addition.addEventListener('click', );
+
+const one = document.getElementById('one');
+
+one.addEventListener('click', );
+
+const two = document.getElementById('two');
+
+two.addEventListener('click', );
+
+const three = document.getElementById('three');
+
+three.addEventListener('click', );
+
+const four = document.getElementById('four');
+
+four.addEventListener('click');
+
+const five = document.getElementById('five');
+
+five.addEventListener('click', );
+
+const six = document.getElementById('six');
+
+six.addEventListener('click', );
+
+const seven = document.getElementById('seven');
+
+seven.addEventListener('click', );
+
+const eight = document.getElementById('eight');
+
+eight.addEventListener('click', );
+
+const nine = document.getElementById('nine');
+
+nine.addEventListener('click', );
+
+const zero = document.getElementById('zero');
+
+zero.addEventListener('click', );
+
+const unaryMinus = document.getElementById('unary-minus');
+
+unaryMinus.addEventListener('click', );
+
+const decimal = document.getElementById('decimal');
+
+decimal.addEventListener('click', );
+
+const equalSign = document.getElementById('equals-sign');
+
+equalSign.addEventListener('click', );
+
+
